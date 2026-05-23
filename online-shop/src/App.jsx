694689204cb1dc7f4279b0f3de33ef1edd12070b
Lost Agent <github@hasanhuseyin.tr>
2026-05-23 10:39:30 +0300

    Updated columns to 4

--- online-shop/src/App.jsx
+++ online-shop/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <div className='app-root min-h-screen min-w-screen'>
       <Header />
       <div className='flex justify-center'>
-      <div className='grid grid-cols-2 gap-5'>
+      <div className='grid grid-cols-4 gap-5'>
         {products.map(product => (
           <div className='w-64 h-64 bg-red-500' key={product.id}>
             <h2>{product.name}</h2>
