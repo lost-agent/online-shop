3e9d288fecd9b6478dde3c68c1307fc4c277bc87
Lost Agent <github@hasanhuseyin.tr>
2026-05-23 10:49:07 +0300

    Update ListItem.jsx

--- online-shop/src/components/ListItem.jsx
+++ online-shop/src/components/ListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const ListItem = ({ product }) => {
   return (
-    <div className="w-64 h-64 bg-red-500" key={product.id}>
+    <div className="w-64 h-64 bg-red-500 rounded-2xl">
       <h2>{product.name}</h2>
     </div>
   );
