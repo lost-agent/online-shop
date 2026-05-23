4ed4be9a854f91e240bec85f263e34d81fe2a00d
Lost Agent <github@hasanhuseyin.tr>
2026-05-23 10:41:20 +0300

    Update App.jsx

--- online-shop/src/App.jsx
+++ online-shop/src/App.jsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
-import './App.css'
-import { Header } from './components/Header'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     async function fetchProducts() {
-      const response = await fetch('/api/products');
+      const response = await fetch("/api/products");
       const data = await response.json();
       setProducts(data);
     }
@@ -16,20 +16,20 @@ function App() {
 
   return (
     <>
-    <div className='app-root min-h-screen min-w-screen'>
-      <Header />
-      <div className='flex justify-center'>
-      <div className='grid grid-cols-4 gap-5'>
-        {products.map(product => (
-          <div className='w-64 h-64 bg-red-500' key={product.id}>
-            <h2>{product.name}</h2>
+      <div className="app-root min-h-screen min-w-screen">
+        <Header />
+        <div className="flex justify-center">
+          <div className="grid grid-cols-4 gap-5 mt-5">
+            {products.map((product) => (
+              <div className="w-64 h-64 bg-red-500" key={product.id}>
+                <h2>{product.name}</h2>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-      </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
