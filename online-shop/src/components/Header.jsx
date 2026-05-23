f35410c7571bf38c64556e20c54864f512071986
Lost Agent <github@hasanhuseyin.tr>
2026-05-23 10:45:02 +0300

    Added stickiness to the header

--- online-shop/src/components/Header.jsx
+++ online-shop/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <header className='w-full flex justify-center text-2xl text-white font-bold'>
+    <header className='w-full flex justify-center text-2xl text-white font-bold sticky top-0'>
         <nav className='w-1/2 bg-blue-500 h-32 rounded-b-2xl flex items-center justify-between pr-10 pl-10'>
           <p>Logo</p>
           <ul className='flex gap-5'>
